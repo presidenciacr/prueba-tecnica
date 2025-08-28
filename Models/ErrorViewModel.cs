2a234bdd27448c629ea28a045af679a33e007469
Enrique Alpízar <me@ealpizar.com>
2025-08-28 13:04:04 -0600

    feat: initialize project

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace PruebaTecnicaPresidencia.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
